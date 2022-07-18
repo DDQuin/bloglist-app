da5af1a82713cf8148f9777afd6cb40fd0cbb88d
DDQuin <ddquin420@gmail.com>
2022-07-18 13:08:36 +0100

    added pipeline

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
+app.get('/health', (req, res) => {
+  res.send('ok');
+});
 app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
